a5580a3d38a23b78b2813f33a206e9f472d89e67
rejl-no-no <96285491+rejl-no-no@users.noreply.github.com>
2023-01-09 16:59:16 +0300

    Update render_website.py

--- render_website.py
+++ render_website.py
@@ -10,24 +10,18 @@ from livereload import shell
 from more_itertools import chunked
 
 
-site = argparse.ArgumentParser(description='Создание сайта по данным из файла .json')
-site.add_argument('--content', default = 'page_content_json.json', help='Файл с данными для создания сайта')
-args = site.parse_args()
-
-
 def rebuild():
-    cards_on_the_page = 10
+    number_of_cards = 10
     number_of_columns = 2
 
     env = Environment(loader=FileSystemLoader('.'), autoescape=select_autoescape(['html', 'xml']))
     template = env.get_template('template.html')
 
     with open(args.content, 'r', encoding='utf8') as file:
-        books_json = file.read()
-    book_descriptions = json.loads(books_json)
+        book_descriptions = json.load(file)
 
     os.makedirs(f'pages', exist_ok=True)
-    pages = list(chunked(book_descriptions, cards_on_the_page))
+    pages = list(chunked(book_descriptions, number_of_cards))
 
     for num, page in enumerate(pages, start = 1):
         rendered_page = template.render(
@@ -49,4 +43,8 @@ def main():
 
 
 if __name__ == '__main__':
+    site = argparse.ArgumentParser(description='Создание сайта по данным из файла .json')
+    site.add_argument('--content', default = 'page_content_json.json', help='Файл с данными для создания сайта')
+    args = site.parse_args()
+    
     main()
